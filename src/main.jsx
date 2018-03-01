803de42fae663d1fcdc172f3f65779ab861af455
LizbethFP <lizbeth.fp@gmail.com>
2018-03-01 01:10:50 -0500

    cambio en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ var ReactDOM = require('react-dom');
 //var Routes = require('./Routes.jsx');
 var list = require('./components/List.jsx');
 
-ReactDOM.render(/*mi componente*/, document.getElementById('main'));
+//ReactDOM.render(/*mi componente*/, document.getElementById('main'));
+ReactDOM.render(<List/>, document.getElementById('main'));
 
 /*
 JSX -> traductor -> JS 
