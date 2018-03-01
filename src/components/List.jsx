3cc126accecfe740ae38b7b4d0c312eedc2627a3
LizbethFP <lizbeth.fp@gmail.com>
2018-03-01 01:09:27 -0500

    intento de esqueleto react

---
+++ src/components/List.jsx
@@ -0,0 +1,10 @@
+let React = require('react');
+let List = React.createClass({
+  render: function() {
+    return (
+			<div>Hola</div>
+		);
+    }
+});
+
+module.exports = List;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+//var Routes = require('./Routes.jsx');
+var list = require('./components/List.jsx');
+
+ReactDOM.render(/*mi componente*/, document.getElementById('main'));
+
+/*
+JSX -> traductor -> JS 
+*/
